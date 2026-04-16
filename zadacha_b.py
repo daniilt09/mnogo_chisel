500b9e77853c901c090ec8d39d249ae1775a66c3
daniilt09 <daniiltimukin207@gmail.com>
2026-04-16 20:10:33 +0700

    Update zadacha_b.py

--- zadacha_b.py
+++ zadacha_b.py
@@ -1,11 +1,7 @@
 import collections
 
-bilo = []
-count = 0
-top = []
-bilos = set()
-
 n = int(input())
+
 
 def proverka(proshloe, chis):
     s1, s2 = str(proshloe), str(chis)
@@ -16,9 +12,10 @@ def proverka(proshloe, chis):
     else:
         long, short = s2, s1
     for i in range(len(long)):
-        if long[:i] + long[i + 1 :] == short:
+        if long[:i] + long[i + 1:] == short:
             return True
     return False
+
 
 prost = [q for q in range(n)]
 prost[1] = 0
@@ -37,14 +34,15 @@ for i in range(len(prost)):
         if proverka(prost[i], prost[j]):
             sosedi[prost[i]].append(prost[j])
             sosedi[prost[j]].append(prost[i])
-
+print(prost)
 print(len(sosedi))
+
 
 def poisk(sosedi, prost):
     bilo = []
     for nachalo in prost:
         bilo.append(([nachalo], {nachalo}))
-    
+
     top = []
     if n <= 1000:
         shirina = 100
@@ -58,23 +56,24 @@ def poisk(sosedi, prost):
         for put, posisheno in bilo:
             if len(put) > len(top):
                 top = put
-            
+
             seichas = put[-1]
             if seichas in sosedi:
                 variki = []
                 for nxt in sosedi[seichas]:
                     if nxt not in posisheno:
                         variki.append(nxt)
-                
+
                 for nxt in variki:
                     novoe_posisheno = set(posisheno)
                     novoe_posisheno.add(nxt)
                     next_bilo.append((put + [nxt], novoe_posisheno))
-        
+
         if not next_bilo:
             break
-            
-        next_bilo.sort(key=lambda x: len([n for n in sosedi[x[0][-1]] if n not in x[1]]), reverse = True)
+
+        next_bilo.sort(key=lambda x: len(
+            [n for n in sosedi[x[0][-1]] if n not in x[1]]), reverse=True)
         bilo = next_bilo[:shirina]
 
     return top
